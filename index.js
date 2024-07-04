1c0b3e698ff6f2fa358b81113bff7e6681d2d7cb
Thong Ta <thanthong.ta@gmail.com>
2024-07-04 14:15:47 +0200

    add nodejs

---
+++ index.js
@@ -0,0 +1,8 @@
+// server code
+import express from 'express'
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
