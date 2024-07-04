0afb21832c4fa7d9ab1475cc7bb32e74124aad82
Thong Ta <thanthong.ta@gmail.com>
2024-07-04 21:14:33 +0200

    add certificates and ssl

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 // server code
 import express from 'express'
+import https from 'https'
+import { readFile } from 'fs/promises'
+import { say } from 'cowsay'
+
+// Prereq:
+// import the crt to the keychain access and 
+// set trust to "always trust"
+
+const {key, cert} = await (async () => {
+  return {
+    key: await readFile(`certs/localhost.key`),
+    cert: await readFile(`certs/localhost.crt`)
+  }
+})();
+
+console.log(say({
+  text: 'Hello, World!',
+  e: 'oO',
+  T: 'U',
+  f: 'small'
+}))
+
 const app = express()
-const port = 3000
-app.use(express.static('public'))
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const port = 4300
+// app.use(express.static('public'))
+https.createServer({key, cert}, app).listen(port, () => {
+  console.log(`Example app listening at https://localhost:${port}`)
+}); 
