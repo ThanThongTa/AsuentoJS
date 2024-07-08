e00ba8d247ef32212c35ec237b477d443d9a7cc5
Thong Ta <thanthong.ta@gmail.com>
2024-07-08 09:38:30 +0200

    add knot formulaes

--- modules/main.js
+++ modules/main.js
@@ -35,6 +35,12 @@ const sketch = function (p5) {
   // Variablen, die über Funktionen von außen geändert werden können
   p5.display = false
 
+  const TORUS = 1
+  const EIGHT = 2
+  const TREFOIL = 3
+  const GRANNY = 4
+  const BRAID = 5
+
   // Variablen, die über Slider und Buttons geändert werden können
   const vectors = []
   const maxVectors = 10000
@@ -44,6 +50,28 @@ const sketch = function (p5) {
   const knotType = 0
   const radius = 50
   const ambientLight = 128 // Werte von 0 bis 255. 0 ist schwarz, 255 ist weiss
+  const trefoilK = 1
+
+  let bias = 0.8
+  let rFactor1 = 1
+  let rFactor2 = 1.6
+  let rPiFactor = 0
+  let rBetaFactor = 6
+  let thetaFactor = 2
+  let phiPiFactor = 0.6
+  let phiBetaFactor = 12
+
+  // Knot 5:
+  if (knotType === 5) {
+    bias = 0
+    rFactor1 = 1.2
+    rFactor2 = 0.6
+    rPiFactor = 0.5
+    rBetaFactor = 6
+    thetaFactor = 4
+    phiPiFactor = 0.2
+    phiBetaFactor = 6
+  }
 
   // Variablen, die innerhalb der Anwendung geöändert werden
   // let angle = 0
@@ -53,16 +81,42 @@ const sketch = function (p5) {
   const calcVector = (type) => {
     let x, y, z
 
-    let r = 0.8 + 1.6 * p5.sin(6 * beta)
-    const theta = 2 * beta
-    const phi = 0.6 * p5.PI * p5.sin(12 * beta)
+    let r = 1
+    let theta = 2 * beta
+    let phi = 2 * p5.PI * p5.sin(6 * beta)
+
+    if (type === TORUS) {
+      r = bias * rFactor1 + rFactor2 * (rPiFactor * p5.PI + p5.sin(rBetaFactor * beta))
+      theta = thetaFactor * beta
+      phi = phiPiFactor * p5.PI * p5.sin(phiBetaFactor * beta)
+    }
 
     r = radius * r
     switch (type) {
-      case 1:
+      case TORUS:
         x = r * p5.cos(phi) * p5.cos(theta)
         y = r * p5.cos(phi) * p5.sin(theta)
         z = r * p5.sin(phi)
+        break
+      case EIGHT:
+        x = 10 * p5.cos(beta) + 10 * p5.cos(3 * beta) + p5.cos(2 * beta) + p5.cos(4 * beta)
+        y = 6 * p5.sin(beta) + 10 * p5.sin(3 * beta)
+        z = 4 * p5.sin(3 * beta) * p5.sin(5 * beta / 2) + 4 * p5.sin(4 * beta) - 2 * p5.sin(6 * beta)
+        break
+      case BRAID:
+        x = 41 * p5.cos(1 * beta) - 18 * p5.sin(1 * beta) - 83 * p5.cos(2 * beta) - 83 * p5.sin(2 * beta) - 11 * p5.cos(3 * beta) + 27 * p5.sin(3 * beta)
+        y = 36 * p5.cos(1 * beta) + 27 * p5.sin(1 * beta) - 113 * p5.cos(2 * beta) + 30 * p5.sin(2 * beta) + 11 * p5.cos(3 * beta) + 27 - p5.sin(3 * beta)
+        z = 0 * p5.cos(1 * beta) + 45 * p5.sin(1 * beta) - 30 * p5.cos(2 * beta) + 113 * p5.sin(2 * beta) - 11 * p5.cos(3 * beta) + 27 * p5.sin(3 * beta)
+        break
+      case TREFOIL:
+        x = p5.cos(beta) * (2 - p5.cos(2 * beta / (2 * trefoilK + 1)))
+        y = p5.sin(beta) * (2 - p5.cos(2 * beta / (2 * trefoilK + 1)))
+        z = -1 * p5.sin(2 * beta / (2 * trefoilK + 1))
+        break
+      case GRANNY:
+        x = -22 * p5.cos(1 * beta) - 128 * p5.sin(1 * beta) - 44 * p5.cos(3 * beta) - 78 * p5.sin(3 * beta)
+        y = -10 * p5.sin(2 * beta) - 27 * p5.cos(2 * beta) + 38 * p5.sin(4 * beta) + 46 * p5.cos(4 * beta)
+        z = 70 * p5.sin(3 * beta) - 40 * p5.cos(3 * beta)
         break
       default:
         x = radius * p5.cos(beta)
@@ -76,16 +130,19 @@ const sketch = function (p5) {
   const calcVectors = (type) => {
     vectors.push(calcVector(type))
 
+    let maxBeta = 2 * p5.PI
+    if (type === TREFOIL) maxBeta = (4 * trefoilK + 2) * p5.PI
+
     // nur eine bestimmte Anzahl an Kugeln zeichnen. Die ältesten werden gelöscht
     if (vectors.length > maxVectors) vectors.shift()
 
     // Beta ist der Winkel, der gerade gezeichnet wird.
     // Beta weitersetzen, für die Animation
     // 2 * PI = 360 Grad
-    beta += 2 * p5.PI / maxVectors
+    beta += maxBeta / maxVectors
 
     // Beta wieder auf 0 setzen, um die Animation wieder von vorne zu starten
-    if (beta >= 2 * p5.PI) {
+    if (beta >= maxBeta) {
       beta = 0
     }
   }
