7455fcb581b28321f9a10425ef918cf2ce242d55
Thong Ta <thanthong.ta@gmail.com>
2024-07-09 11:06:13 +0200

    created knot classes and start using them

---
+++ modules/knot.js
@@ -0,0 +1,318 @@
+export class Knot {
+  constructor (p5, maxVectors, radius) {
+    this.radius = radius
+    this.vectors = []
+    this.maxVectors = maxVectors
+    this.beta = 0
+    this.p5 = p5
+  }
+}
+
+export class TorusKnot extends Knot {
+  constructor (p5, maxVectors, radius, r, p, px, q, nx, ny, m, phi, trefoilK) {
+    super(p5, maxVectors, radius)
+    this.r0 = this.radius
+    this.r = r || 0
+    this.p = p || 0
+    this.q = q || 1
+    this.m = m || 0
+    this.px = px || 0
+    this.nx = nx || 1
+    this.ny = ny || 1
+    this.phi = phi || 0
+    this.trefoilK = trefoilK || 0
+    if (this.trefoilK !== 0) this.p = 2 / (2 * trefoilK + 1)
+  }
+
+  getVectors () {
+    this.vectors.push(this.calcVector())
+    const maxBeta = (4 * this.trefoilK + 2) * this.p5.PI
+
+    // nur eine bestimmte Anzahl an Kugeln zeichnen. Die ältesten werden gelöscht
+    if (this.vectors.length > this.maxVectors) this.vectors.shift()
+
+    // Beta ist der Winkel, der gerade gezeichnet wird.
+    // Beta weitersetzen, für die Animation
+    // 2 * PI = 360 Grad
+    this.beta += maxBeta / this.maxVectors
+
+    // Beta wieder auf 0 setzen, um die Animation wieder von vorne zu starten
+    if (this.beta >= maxBeta) {
+      this.beta = 0
+    }
+
+    return this.vectors
+  }
+
+  calcVector () {
+    let phiValue = this.p * this.m * this.beta
+    if (this.phi === 1) phiValue = 0.6 * this.p5.PI * this.p5.sin(2 * this.p * this.beta)
+    if (this.phi === 2) phiValue = 0.2 * this.p5.PI * this.p5.sin(this.p * this.beta)
+    const x = (this.r0 + this.r * this.p5.cos(this.px + this.p * this.beta)) * this.p5.cos(this.q * this.beta) * this.nx
+    const y = (this.r0 + this.r * this.p5.cos(this.p * this.beta)) * this.p5.sin(this.q * this.beta) * this.ny
+    const z = this.r * this.p5.sin(phiValue)
+    return this.p5.createVector(x, y, z)
+  }
+}
+
+export class LissaKnot extends Knot {
+  constructor (p5, maxVectors, radius, nx, ny, nz, bx, by, bz, nz2) {
+    super(p5, maxVectors, radius)
+    this.nx = nx || 1
+    this.ny = ny || 1
+    this.nz = nz || 1
+    this.bx = bx || 0
+    this.by = by || 0
+    this.bz = bz || 0
+    this.nz2 = nz2 || 0
+  }
+
+  getVectors () {
+    this.vectors.push(this.calcVector())
+    const maxBeta = 2 * this.p5.PI
+
+    // nur eine bestimmte Anzahl an Kugeln zeichnen. Die ältesten werden gelöscht
+    if (this.vectors.length > this.maxVectors) this.vectors.shift()
+
+    // Beta ist der Winkel, der gerade gezeichnet wird.
+    // Beta weitersetzen, für die Animation
+    // 2 * PI = 360 Grad
+    this.beta += maxBeta / this.maxVectors
+
+    // Beta wieder auf 0 setzen, um die Animation wieder von vorne zu starten
+    if (this.beta >= maxBeta) {
+      this.beta = 0
+    }
+
+    return this.vectors
+  }
+
+  calcVector () {
+    const x = this.p5.cos(this.nx * this.beta + this.bx)
+    const y = this.p5.cos(this.ny * this.beta + this.by)
+    const z = this.p5.cos(this.nz * this.beta + this.bz) + this.p5.cos(this.nz2 * this.beta)
+    return this.p5.createVector(x, y, z)
+  }
+}
+
+export class CosStackKnot extends Knot {
+  constructor (p5, maxVectors, radius, options) {
+    super(p5, maxVectors, radius)
+    this.options = options
+  }
+
+  getVectors () {
+    this.vectors.push(this.calcVector())
+    const maxBeta = 2 * this.p5.PI
+
+    // nur eine bestimmte Anzahl an Kugeln zeichnen. Die ältesten werden gelöscht
+    if (this.vectors.length > this.maxVectors) this.vectors.shift()
+
+    // Beta ist der Winkel, der gerade gezeichnet wird.
+    // Beta weitersetzen, für die Animation
+    // 2 * PI = 360 Grad
+    this.beta += maxBeta / this.maxVectors
+
+    // Beta wieder auf 0 setzen, um die Animation wieder von vorne zu starten
+    if (this.beta >= maxBeta) {
+      this.beta = 0
+    }
+
+    return this.vectors
+  }
+
+  calcVector () {
+    let zoffset = 0
+    if (this.options.zoffset) zoffset = this.p5.sin(2.5 * this.beta) - 2 * this.p5.sin(6 * this.beta)
+    const x = this.options.xc1 * this.p5.cos(1 * this.beta) + this.options.xs1 * this.p5.sin(1 * this.beta) +
+    this.options.xc2 * this.p5.cos(2 * this.beta) + this.options.xs2 * this.p5.sin(2 * this.beta) +
+    this.options.xc3 * this.p5.cos(3 * this.beta) + this.options.xs3 * this.p5.sin(3 * this.beta) +
+    this.options.xc4 * this.p5.cos(4 * this.beta) + this.options.xs4 * this.p5.sin(4 * this.beta)
+    const y = this.options.yc1 * this.p5.cos(1 * this.beta) + this.options.ys1 * this.p5.sin(1 * this.beta) +
+    this.options.yc2 * this.p5.cos(2 * this.beta) + this.options.ys2 * this.p5.sin(2 * this.beta) +
+    this.options.yc3 * this.p5.cos(3 * this.beta) + this.options.ys3 * this.p5.sin(3 * this.beta) +
+    this.options.yc4 * this.p5.cos(4 * this.beta) + this.options.ys4 * this.p5.sin(4 * this.beta)
+    const z = this.options.zc1 * this.p5.cos(1 * this.beta) + this.options.zs1 * this.p5.sin(1 * this.beta) +
+    this.options.zc2 * this.p5.cos(2 * this.beta) + this.options.zs2 * this.p5.sin(2 * this.beta) +
+    this.options.zc3 * this.p5.cos(3 * this.beta) + this.options.zs3 * this.p5.sin(3 * this.beta) +
+    this.options.zc4 * this.p5.cos(4 * this.beta) + this.options.zs4 * this.p5.sin(4 * this.beta) + zoffset
+    return this.p5.createVector(x, y, z)
+  }
+}
+
+export class CosArguments {
+  constructor () {
+    this.xc1 = 0
+    this.xs1 = 0
+    this.xc2 = 0
+    this.xs2 = 0
+    this.xc3 = 0
+    this.xs3 = 0
+    this.xc4 = 0
+    this.xs4 = 0
+    this.yc1 = 0
+    this.ys1 = 0
+    this.yc2 = 0
+    this.ys2 = 0
+    this.yc3 = 0
+    this.ys3 = 0
+    this.yc4 = 0
+    this.ys4 = 0
+    this.zc1 = 0
+    this.zs1 = 0
+    this.zc2 = 0
+    this.zs2 = 0
+    this.zc3 = 0
+    this.zs3 = 0
+    this.zc4 = 0
+    this.zs4 = 0
+    this.xoffset = 0
+    this.yoffset = 0
+    this.zoffset = 0
+  }
+}
+
+export class KnotTypes {
+  static UNKNOT = 0
+  static TORUS = 1
+  static EIGHT = 2
+  static TREFOIL = 3
+  static GRANNY = 4
+  static CINQUEFOIL = 5
+  static CLOVER = 6
+  static TREFOIL2 = 7
+  static EIGHTS = 9
+  static THREETWIST = 10
+  static STEVEDORE = 11
+  static GRANNY2 = 12
+  static CARRICK = 13
+  static LISSAJOUS5 = 14
+  static SEPTAFOIL = 15
+  static TREFOIL3 = 16
+  static EIGHT3 = 17
+  static CINQUEFOIL2 = 18
+  static TREFOIL4 = 19
+  static TORUS3 = 20
+  static KNOT5 = 21
+  static THREETWIST2 = 22
+}
+
+export class KnotFactory {
+  static createKnot (p5, maxVectors, radius, knotType) {
+    const options = new CosArguments()
+    let knot
+
+    switch (knotType) {
+      case KnotTypes.TORUS:
+        knot = new TorusKnot(p5, maxVectors, 0.8 * radius, 1.6, 6, 0, 2, 1, 1, 1, 1, 0)
+        break
+      case KnotTypes.KNOT5:
+        knot = new TorusKnot(p5, maxVectors, 0, 0.72, 6, 0, 4, 1, 1, 1, 2, 0)
+        break
+      case KnotTypes.TREFOIL:
+        knot = new TorusKnot(p5, maxVectors, 0, 1, 1, 0, 2, 2, 2, 3, 0, 0)
+        break
+      case KnotTypes.TREFOIL2:
+        knot = new TorusKnot(p5, maxVectors, 2, 1, 3, 0, 1, 1, 1, 1, 0, 0)
+        break
+      case KnotTypes.TREFOIL4:
+        knot = new TorusKnot(p5, maxVectors, 3, 1, 3, 0, 2, 1, 1, 1, 0, 0)
+        break
+      case KnotTypes.TORUS3:
+        knot = new TorusKnot(p5, maxVectors, 3, 1, 4, 0, 3, 1, 1, 1, 0, 0)
+        break
+      case KnotTypes.CINQUEFOIL:
+        knot = new TorusKnot(p5, maxVectors, 2, -1, 2, 0, 1, 0, 0, 1, 0, 1)
+        break
+      case KnotTypes.CINQUEFOIL2:
+        knot = new TorusKnot(p5, maxVectors, 3, 1, 5, 0, 2, 1, 1, 1, 0, 0)
+        break
+      case KnotTypes.EIGHT:
+        knot = new TorusKnot(p5, maxVectors, 2, 1, 2, 0, 3, 1, 1, 2, 0, 0)
+        break
+      case KnotTypes.TREFOIL3:
+        knot = new LissaKnot(p5, maxVectors, radius, 2, 2, 4, 0.8, 0.15, 1, 5)
+        break
+      case KnotTypes.EIGHT3:
+        knot = new LissaKnot(p5, maxVectors, radius, 2, 2, 6, 6, 0.15, 1, 5)
+        break
+      case KnotTypes.THREETWIST:
+        knot = new LissaKnot(p5, maxVectors, radius, 3, 2, 7, 0.7, 0.2, 0, 0)
+        break
+      case KnotTypes.THREETWIST2:
+        knot = new LissaKnot(p5, maxVectors, radius, 3, 2, 7, 1.0, 0.4, 0, 0)
+        break
+      case KnotTypes.STEVEDORE:
+        knot = new LissaKnot(p5, maxVectors, radius, 3, 2, 5, 1.5, 0.2, 0, 0)
+        break
+      case KnotTypes.GRANNY2:
+        knot = new LissaKnot(p5, maxVectors, radius, 3, 5, 7, 0.7, 1.0, 0, 0)
+        break
+      case KnotTypes.CARRICK:
+        knot = new LissaKnot(p5, maxVectors, radius, 3, 4, 7, 0.1, 0.7, 0, 0)
+        break
+      case KnotTypes.LISSAJOUS5:
+        knot = new LissaKnot(p5, maxVectors, radius, 2, 3, 7, 0.22, 1.10, 0, 0)
+        break
+      case KnotTypes.EIGHTS:
+        options.xc1 = 10
+        options.xc2 = 1
+        options.xc3 = 10
+        options.xc4 = 1
+        options.ys1 = 6
+        options.ys3 = 10
+        options.zs3 = 4
+        options.zs4 = 4
+        options.zoffset = 1
+        knot = new CosStackKnot(p5, maxVectors, radius, options)
+        break
+      case KnotTypes.CLOVER:
+        options.xc1 = 4 / 3
+        options.xc3 = 2
+        options.yc1 = 4 / 3
+        options.yc3 = 2
+        options.zs2 = 0.5
+        options.zs4 = 1
+        knot = new CosStackKnot(p5, maxVectors, radius, options)
+        break
+      case KnotTypes.GRANNY: // COMPOSITE
+        options.xc1 = -22
+        options.xs1 = -128
+        options.xc3 = -44
+        options.xs3 = -78
+        options.yc2 = -10
+        options.ys2 = -27
+        options.ys4 = 38
+        options.yc4 = 46
+        options.zs3 = 70
+        options.zc3 = -40
+        knot = new CosStackKnot(p5, maxVectors, radius, options)
+        break
+      case KnotTypes.SEPTAFOIL:
+        options.xc1 = 41
+        options.xs1 = -18
+        options.xc2 = -83
+        options.xs2 = -83
+        options.xc3 = -11
+        options.xs3 = 27
+        options.yc1 = 36
+        options.ys1 = 27
+        options.yc2 = -113
+        options.ys2 = 30
+        options.yc3 = 11
+        options.ys3 = 27
+        options.zs1 = 45
+        options.zc2 = -30
+        options.zs2 = 113
+        options.zc3 = -11
+        options.zs3 = 27
+        knot = new CosStackKnot(p5, maxVectors, radius, options)
+        break
+      case KnotTypes.UNKNOT:
+      default:
+        knot = new TorusKnot(p5, maxVectors, radius, 0, 0, 0, 1, 1, 1, 1, 0, 0)
+        break
+    }
+    return knot
+  }
+}
--- modules/main.js
+++ modules/main.js
@@ -6,315 +6,30 @@
 // https://paulbourke.net/geometry/knots/
 
 /* global p5 */
+import { KnotFactory, KnotTypes } from './knot.js'
 
 const sketch = function (p5) {
   // Variablen, die über Funktionen von außen geändert werden können
   p5.display = false
 
-  const UNKNOT = 0
-  const TORUS = 1
-  const EIGHT = 2
-  const TREFOIL = 3
-  const GRANNY = 4
-  const CINQUEFOIL = 5
-  const CLOVER = 6
-  const TREFOIL2 = 7
-  const EIGHTS = 9
-  const THREETWIST = 10
-  const STEVEDORE = 11
-  const GRANNY2 = 12
-  const CARRICK = 13
-  const LISSAJOUS5 = 14
-  const SEPTAFOIL = 15
-  const TREFOIL3 = 16
-  const EIGHT3 = 17
-  const CINQUEFOIL2 = 18
-  const TREFOIL4 = 19
-  const TORUS3 = 20
-  const KNOT5 = 21
-
   // Variablen, die über Slider und Buttons geändert werden können
-  const vectors = []
+  let vectors = []
   const maxVectors = 10000
   const sphereRadius = 2.5
   const materialAlpha = 0.5
   const drawSpeed = 300 // Anzahl der Kugeln, die gleichzeitig gezeichnet werden
-  const knotType = UNKNOT
+  const knotType = KnotTypes.UNKNOT
   const radius = 50
   const ambientLight = 128 // Werte von 0 bis 255. 0 ist schwarz, 255 ist weiss
-  const trefoilK = 1
-
-  let r0 = 1
-  let p = 2 // longintude
-  let q = 3 // meridian
-  let m = 1
-  let r = 1
 
   // Variablen, die innerhalb der Anwendung geöändert werden
   // let angle = 0
-  let beta = 0
-
-  // berechnet einen einzelnen Punkt des Knotens
-  const calcVector = (type) => {
-    let x, y, z
-    let nx, ny, nz, bx, by, bz, nz2, phi, px
-
-    switch (type) {
-      case TORUS:
-        r0 = 0.8
-        r = 1.6
-        m = 1
-        p = 6
-        q = 2
-        px = 0
-        nx = 1
-        ny = 1
-        phi = 0.6 * p5.PI * p5.sin(2 * p * beta)
-        break
-      case KNOT5:
-        r0 = 0
-        r = 0.72
-        m = 1
-        p = 6
-        q = 4
-        px = 0
-        nx = 1
-        ny = 1
-        phi = 0.2 * p5.PI * p5.sin(p * beta)
-        break
-      case TREFOIL:
-        r0 = 0
-        m = 3
-        r = 1
-        p = 1
-        q = 2
-        px = 0
-        nx = 2
-        ny = 2
-        phi = p * m * beta
-        break
-      case TREFOIL2:
-        r0 = 2
-        m = 1
-        r = 1
-        p = 3
-        q = 1
-        px = 0
-        nx = 1
-        ny = 1
-        phi = p * m * beta
-        break
-      case TREFOIL4:
-        r0 = 3
-        m = 1
-        r = 1
-        p = 3
-        q = 2
-        px = 0
-        nx = 1
-        ny = 1
-        phi = p * m * beta
-        break
-      case TORUS3:
-        r0 = 3
-        m = 1
-        r = 1
-        p = 4
-        q = 3
-        px = 0
-        nx = 1
-        ny = 1
-        phi = p * m * beta
-        break
-      case CINQUEFOIL2:
-        r0 = 3
-        m = 1
-        r = 1
-        p = 5
-        q = 2
-        px = 0
-        nx = 1
-        ny = 1
-        phi = p * m * beta
-        break
-      case EIGHT:
-        r0 = 2
-        m = 2
-        r = 1
-        p = 2
-        q = 3
-        px = 0
-        nx = 1
-        ny = 1
-        phi = p * m * beta
-        break
-      case UNKNOT:
-      default:
-        r0 = radius
-        r = 0
-        p = 0
-        q = 1
-        m = 0
-        px = 0
-        nx = 1
-        ny = 1
-        phi = 0
-        break
-    }
-
-    switch (type) {
-      case TREFOIL3:
-        nx = 2
-        ny = 2
-        nz = 4
-        bx = 0.8
-        by = 0.15
-        bz = 1
-        nz2 = 5
-        break
-      case EIGHT3:
-        nx = 2
-        ny = 2
-        nz = 6
-        bx = 6
-        by = 0.15
-        bz = 1
-        nz2 = 5
-        break
-      case THREETWIST:
-        nx = 3
-        ny = 2
-        nz = 7
-        bx = 0.7 // 1.0
-        by = 0.2 // 0.4
-        bz = 0
-        nz2 = 0
-        break
-      case STEVEDORE:
-        nx = 3
-        ny = 2
-        nz = 5
-        bx = 1.5
-        by = 0.2
-        bz = 0
-        nz2 = 0
-        break
-      case GRANNY:
-        nx = 3
-        ny = 5
-        nz = 7
-        bx = 0.7
-        by = 1.0
-        bz = 0
-        nz2 = 0
-        break
-      case CARRICK:
-        nx = 3
-        ny = 4
-        nz = 7
-        bx = 0.1
-        by = 0.7
-        bz = 0
-        nz2 = 0
-        break
-      case LISSAJOUS5:
-        nx = 2
-        ny = 3
-        nz = 7
-        bx = 0.22
-        by = 1.10
-        bz = 0
-        nz2 = 0
-        break
-      default:
-        nx = 1
-        ny = 1
-        nz = 1
-        bx = 0
-        by = 0
-        bz = 0
-        nz2 = 0
-        break
-    }
-
-    switch (type) {
-      case TORUS:
-      case KNOT5:
-      case UNKNOT:
-      case TREFOIL:
-      case TREFOIL2:
-      case TREFOIL4:
-      case TORUS3:
-      case CINQUEFOIL2:
-      case EIGHT:
-        x = (r0 + r * p5.cos(px + p * beta)) * p5.cos(q * beta) * nx
-        y = (r0 + r * p5.cos(p * beta)) * p5.sin(q * beta) * ny
-        z = r * p5.sin(phi)
-        break
-      case TREFOIL3: // 3 / 2
-      case EIGHT3: // 3 / 2
-      case THREETWIST: // 5 / 2
-      case STEVEDORE: // 6 / 1
-      case GRANNY2: // 3/1 # 3/1
-      case CARRICK: // 8/21 // Carrock set
-      case LISSAJOUS5:
-        x = p5.cos(nx * beta + bx)
-        y = p5.cos(ny * beta + by)
-        z = p5.cos(nz * beta + bz) + p5.cos(nz2 * beta)
-        break
-      case EIGHTS:
-        x = 10 * p5.cos(beta) + p5.cos(2 * beta) + 10 * p5.cos(3 * beta) + p5.cos(4 * beta)
-        y = 6 * p5.sin(beta) + 0 * p5.sin(2 * beta) + 10 * p5.sin(3 * beta)
-        z = 4 * p5.sin(3 * beta) + p5.sin(2.5 * beta) + 4 * p5.sin(4 * beta) - 2 * p5.sin(6 * beta)
-        break
-      case CLOVER:
-        x = 4 / 3 * p5.cos(beta) + 2 * p5.cos(3 * beta)
-        y = 4 / 3 * p5.sin(beta) + 2 * p5.sin(3 * beta)
-        z = 0.5 * p5.sin(2 * beta) + p5.sin(4 * beta)
-        break
-      case SEPTAFOIL:
-        x = 41 * p5.cos(1 * beta) - 18 * p5.sin(1 * beta) - 83 * p5.cos(2 * beta) - 83 * p5.sin(2 * beta) - 11 * p5.cos(3 * beta) + 27 * p5.sin(3 * beta)
-        y = 36 * p5.cos(1 * beta) + 27 * p5.sin(1 * beta) - 113 * p5.cos(2 * beta) + 30 * p5.sin(2 * beta) + 11 * p5.cos(3 * beta) + 27 - p5.sin(3 * beta)
-        z = 0 * p5.cos(1 * beta) + 45 * p5.sin(1 * beta) - 30 * p5.cos(2 * beta) + 113 * p5.sin(2 * beta) - 11 * p5.cos(3 * beta) + 27 * p5.sin(3 * beta)
-        break
-      case CINQUEFOIL:
-        x = p5.cos(beta) * (2 - p5.cos(2 * beta / (2 * trefoilK + 1)))
-        y = p5.sin(beta) * (2 - p5.cos(2 * beta / (2 * trefoilK + 1)))
-        z = -1 * p5.sin(2 * beta / (2 * trefoilK + 1))
-        break
-      case GRANNY: // COMPOSITE
-        x = -22 * p5.cos(1 * beta) - 128 * p5.sin(1 * beta) - 44 * p5.cos(3 * beta) - 78 * p5.sin(3 * beta)
-        y = -10 * p5.sin(2 * beta) - 27 * p5.cos(2 * beta) + 38 * p5.sin(4 * beta) + 46 * p5.cos(4 * beta)
-        z = 70 * p5.sin(3 * beta) - 40 * p5.cos(3 * beta)
-        break
-    }
-    return p5.createVector(x, y, z)
-  }
-
-  // berechnet alle Punkte eines Knotens
-  const calcVectors = (type) => {
-    vectors.push(calcVector(type))
-
-    let maxBeta = 2 * p5.PI
-    if (type === CINQUEFOIL) maxBeta = (4 * trefoilK + 2) * p5.PI
-
-    // nur eine bestimmte Anzahl an Kugeln zeichnen. Die ältesten werden gelöscht
-    if (vectors.length > maxVectors) vectors.shift()
-
-    // Beta ist der Winkel, der gerade gezeichnet wird.
-    // Beta weitersetzen, für die Animation
-    // 2 * PI = 360 Grad
-    beta += maxBeta / maxVectors
-
-    // Beta wieder auf 0 setzen, um die Animation wieder von vorne zu starten
-    if (beta >= maxBeta) {
-      beta = 0
-    }
-  }
+  let knot
 
   // p5 Funktion, die vor dem Zeichnen aufgerufen wird
   p5.setup = function () {
     p5.createCanvas(400, 400, p5.WEBGL)
+    knot = KnotFactory.createKnot(p5, maxVectors, radius, knotType)
   }
 
   // Loop-Funktion, die immer wieder aufgerufen wird
@@ -329,7 +44,7 @@ const sketch = function (p5) {
 
     // Kugeln berechnen
     for (let i = 0; i < drawSpeed; i++) {
-      calcVectors(knotType)
+      vectors = knot.getVectors()
     }
 
     // Kugeln zeichnen
