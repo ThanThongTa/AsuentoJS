8256837ecc2e66f6a6b14fc46e72769a112958cb
Thong Ta <thanthong.ta@gmail.com>
2024-07-10 14:23:27 +0200

    add stuff to right-box and formatting

--- modules/db_read.js
+++ modules/db_read.js
@@ -1,5 +1,5 @@
 import { db } from './db.js'
-import { create } from './lib.js'
+import { create, el } from './lib.js'
 import { initDB } from './db_save.js'
 
 export const loadKnots = async () => {
@@ -14,6 +14,44 @@ export const loadKnots = async () => {
 
 function renderKnotList (dbData) {
   const wrapper = create('div')
+  dbData.forEach(element => {
+    const row = create('div')
+    row.className = 'knot'
+    wrapper.append(row)
 
+    const knotName = create('span')
+    knotName.className = 'knotname'
+    knotName.innerText = element.name
+    row.append(knotName)
+
+    const knotLoad = create('button')
+    knotLoad.className = 'loader'
+    knotLoad.innerText = 'Load'
+    knotLoad.addEventListener('click', () => {
+      loadKnot(element)
+    })
+    row.append(knotLoad)
+
+    const knotDelete = create('button')
+    knotDelete.className = 'delete'
+    knotDelete.innerText = 'X'
+    knotDelete.addEventListener('click', () => {
+      deleteKnot(element)
+    })
+    row.append(knotDelete)
+  })
   return wrapper
+}
+
+function loadKnot (knot) {
+  console.log(knot)
+  const knotData = db.readItem(knot.id)
+  el('#name').value = knotData.name
+  el('#maxVectors').value = knotData.maxVectors
+  el('#radius').value = knotData.radius
+}
+
+function deleteKnot (knot) {
+  db.deleteItem(knot.id)
+  loadKnots()
 }
